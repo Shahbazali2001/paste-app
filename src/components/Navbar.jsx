a29bdbaf48170c1a94e807fb6beb35b225fcaab3
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-05-18 23:36:42 +0500

    Navbar Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="flex justify-center gap-4 bg-slate-500 text-white p-4">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/pastes">Pastes</NavLink>
+    </nav>
   )
 }
 
