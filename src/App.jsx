88189274440c5971ae98986f4c187e71af6ac761
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-05-18 15:02:56 +0500

    Routes Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-
 import './App.css'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Paste from './components/Paste'
+import ViewPaste from './components/ViewPaste'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/pastes",
+      element: <Paste />,
+    },
+    {
+      path: "/pastes/:id",
+      element: <ViewPaste />,
+    },
+  ]);
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div>
+    <Navbar/>
+    <RouterProvider router={router} />
+    </div>
   )
+  
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { store } from './app/store'
-import {Provider} from 'react'
+import { Provider } from 'react-redux';
 import './index.css'
 import App from './App.jsx'
 
