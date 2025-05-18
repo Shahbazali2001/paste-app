08e5a9464b2669338745bad6eafb9870e735cfdc
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-05-18 13:32:09 +0500

    Store Created

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { store } from './app/store'
+import {Provider} from 'react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+  )
